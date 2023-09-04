e99fde2b148fc4062872bfe10cc350cceb88f54d
aDeus <aleks-dskcomp@yandex.ru>
2023-09-04 15:12:00 +0300

    add Functional

--- app/src/main/java/com/example/weatherds/MainActivity.kt
+++ app/src/main/java/com/example/weatherds/MainActivity.kt
@@ -1,46 +1,101 @@
 package com.example.weatherds
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherds.ui.theme.WeatherDsTheme
+import androidx.compose.ui.unit.dp
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
+const val API_WEATHER = "608c9bc731984dcc81d112401230409"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherDsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        Greeting(cityName = "Moscow", this )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting(cityName: String, context: Context) {
+    val state = remember {
+        mutableStateOf("Неизвестно")
+    }
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+
+
+        ){
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "Температура в $cityName = ${state.value}"
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.BottomCenter
+
+            ){
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+
+                    onClick = {
+                        weatherResult(cityName, state, context )
+
+                }) {
+                Text(text = "Refresh")
+                }
+            }
+        }
+}
+private fun weatherResult(cityName: String, state: MutableState<String> ,context: Context){
+    val url = "https://api.weatherapi.com/v1/current.json" +
+            "?key=$API_WEATHER&" +
+            "q=$cityName" +
+            "&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+                response ->
+            val obj = JSONObject(response)
+            state.value=obj.getJSONObject("current").getString("temp_c") + "C"
+
+        },{
+                error ->
+                Log.d ("MyLog", "Error $error")
+        }
+
     )
+    queue.add(stringRequest)
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherDsTheme {
-        Greeting("Android")
-    }
-}
