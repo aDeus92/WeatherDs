50c57823ccb0b8c95873b5df35c580c7ba3e3e76
aDeus <aleks-dskcomp@yandex.ru>
2023-09-05 19:30:14 +0300

    add Полный тестовый дизайн

--- app/src/main/java/com/example/weatherds/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherds/screens/MainScreen.kt
@@ -1,30 +1,20 @@
 package com.example.weatherds.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -34,7 +24,6 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.weatherds.R
 import com.example.weatherds.ui.theme.BlueLight
-import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
@@ -42,26 +31,22 @@ import kotlinx.coroutines.launch
 fun MainCard() {
 
     Column(
-        modifier = Modifier
-            .padding(5.dp)
+        modifier = Modifier.padding(5.dp)
     ) {
         Card(
 
-            modifier = Modifier
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
             shape = RoundedCornerShape(10.dp),
             colors = CardDefaults.cardColors(containerColor = BlueLight)
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
 
             ) {
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
@@ -70,8 +55,7 @@ fun MainCard() {
                         style = TextStyle(fontSize = 15.sp),
                         color = Color.White,
                         modifier = Modifier.padding(
-                            start = 8.dp,
-                            top = 8.dp
+                            start = 8.dp, top = 8.dp
                         )
 
                     )
@@ -80,38 +64,28 @@ fun MainCard() {
                         contentDescription = "img",
                         modifier = Modifier
                             .padding(
-                                start = 8.dp,
-                                top = 8.dp,
-                                end = 8.dp
+                                start = 8.dp, top = 8.dp, end = 8.dp
                             )
                             .size(35.dp)
                     )
                 }
                 Text(
-                    text = "Ставрополь",
-                    style = TextStyle(fontSize = 25.sp),
-                    color = Color.White
+                    text = "Ставрополь", style = TextStyle(fontSize = 25.sp), color = Color.White
                 )
                 Text(
-                    text = "26°C",
-                    style = TextStyle(fontSize = 100.sp),
-                    color = Color.White
+                    text = "26°C", style = TextStyle(fontSize = 100.sp), color = Color.White
                 )
 
                 Text(
-                    text = "Воняет",
-                    style = TextStyle(fontSize = 15.sp),
-                    color = Color.White
+                    text = "Воняет", style = TextStyle(fontSize = 15.sp), color = Color.White
                 )
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    IconButton(
-                        onClick =
-                        {
-                            /*TODO*/
-                        }) {
+                    IconButton(onClick = {
+                        /*TODO*/
+                    }) {
                         Icon(
                             painter = painterResource(id = R.drawable.search_24),
                             contentDescription = "img",
@@ -124,11 +98,9 @@ fun MainCard() {
                         style = TextStyle(fontSize = 10.sp),
                         color = Color.White
                     )
-                    IconButton(
-                        onClick =
-                        {
-                            /*TODO*/
-                        }) {
+                    IconButton(onClick = {
+                        /*TODO*/
+                    }) {
                         Icon(
                             painter = painterResource(id = R.drawable.refresh_24),
                             contentDescription = "img",
@@ -143,57 +115,3 @@ fun MainCard() {
 }
 
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun TabLayout() {
-    val tabList = listOf("Часы", "Дни")
-    val pagerState: PagerState = rememberPagerState(
-        initialPage = 0,
-        initialPageOffsetFraction = 0f
-    ) {
-       tabList.size
-    }
-    val tabIndex = pagerState.currentPage
-    val coroutineScope = rememberCoroutineScope()
-    Column(
-        modifier = Modifier
-            .padding(5.dp)
-            .clip(RoundedCornerShape(5.dp)),
-    )
-    {
-        TabRow(
-            selectedTabIndex = tabIndex,
-            containerColor = BlueLight,
-            contentColor = Color.White
-        ) {
-            tabList.forEachIndexed { index, text ->
-                Tab(
-                    selected = pagerState.currentPage == index,
-                    onClick = {
-                        coroutineScope.launch {
-                            pagerState.animateScrollToPage(index)
-                        }
-                    },
-                    text =
-                    {
-                        Text(text = text)
-                    }
-
-                )
-            }
-        }
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.weight(1.0f)
-        ) {
-            index ->
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-            ){
-                items(15){
-                    ListItem()
-                }
-            }
-        }
-    }
-}
---
+++ app/src/main/java/com/example/weatherds/screens/TabLayout.kt
@@ -0,0 +1,77 @@
+package com.example.weatherds.screens
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.weatherds.ui.theme.BlueLight
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("Часы", "Дни")
+    val pagerState: PagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f
+    ) {
+        tabList.size
+    }
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+            .clip(RoundedCornerShape(5.dp)),
+    )
+    {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            containerColor = BlueLight,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text =
+                    {
+                        Text(text = text)
+                    }
+
+                )
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) {
+                index ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ){
+                items(15){
+                    ListItem()
+                }
+            }
+        }
+    }
+}
